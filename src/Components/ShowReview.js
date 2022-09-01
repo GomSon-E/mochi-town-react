a7bac74e557b7431564f6ef6236e4ec6760ab1c5
GomSon-E <gomsonixx@gmail.com>
2022-09-02 02:13:23 +0900

    Complete review.css

--- src/Components/ShowReview.js
+++ src/Components/ShowReview.js
@@ -1,10 +1,11 @@
 function Reviews ({review}) {
   return (
     <li>
+      <img className="img" src={`${process.env.PUBLIC_URL}/products/${review.product}.jpg`} alt={review.product} />
       <dl>
-        <dt>{review.product}</dt>
-        <dd>{review.date}</dd>
-        <dd>{review.note}</dd>
+        <dt className="product">{review.product}</dt>
+        <dd className="date"><dfn>작성일 : </dfn>{review.date}</dd>
+        <dd className="note">{review.note}</dd>
       </dl>
     </li>
   )
--- src/Pages/Review.js
+++ src/Pages/Review.js
@@ -3,6 +3,8 @@ import React, {useState, useEffect, useCallback} from "react";
 import ShowReview from "../Components/ShowReview"
 import WriteReview from "../Components/WriteReview"
 
+import '../css/review.css'
+
 function Review() {
 
   // Toggle
@@ -26,7 +28,7 @@ function Review() {
   const filterReviews = reviewList.filter( review => review.product.includes(query) )
 
   return (
-    <article>
+    <article id="review">
 
       {/* header */}
       <header>
@@ -39,30 +41,37 @@ function Review() {
         </div>
       </header>
 
-      <h3>상품 사용후기</h3>
-      <p>&hearts;모찌타운 회원 적립금지급&#40;포토 1,000원/ 텍스트 500원&#41;&hearts;</p>
+      <article id="reviewMain">
 
-      <label htmlFor="search">상품명 검색</label>
-      <input type="text" id="search" placeholder="상품명 검색" value={tempQuery}
-             onChange={ event => {setTempQuery(event.target.value)} } />
-      <button onClick={() => {setQuery(tempQuery); setTempQuery("");}}>검색</button>
+        <h3>상품 사용후기</h3>
+        <p>&hearts;모찌타운 회원 적립금지급&#40;포토 1,000원/ 텍스트 500원&#41;&hearts;</p>
 
-      <ul>
-        {
-          filterReviews.map(
-            (review) => {return (<ShowReview key={review.id} review={review} /> )})
-        }
-      </ul>
+        <ul id="showReview">
+          {
+            filterReviews.map(
+              (review) => {return (<ShowReview key={review.id} review={review} /> )})
+          }
+        </ul>
 
-      <button onClick={() => {setToggle(!toggle)}}>글쓰기</button>
-      <WriteReview 
-        toggle={toggle} setToggle={setToggle}
-        lastId={
-          reviewList.reduce((max, review) => review.id > max ? review.id : max, 0 ) 
-        }
-        onAddReview={
-          (myReview) => { setReviewList([...reviewList, myReview]) }
-        } />
+        <div id="searchReview">
+          <label htmlFor="search">상품명 검색</label>
+          <input type="text" id="search"  value={tempQuery}
+                onChange={ event => {setTempQuery(event.target.value)} } />
+          <button onClick={() => {setQuery(tempQuery); setTempQuery("");}}>검색</button>
+        </div>        
+
+        <div id="writeReview">
+          <button onClick={() => {setToggle(!toggle)}}>글쓰기</button>
+          <WriteReview 
+            toggle={toggle} setToggle={setToggle}
+            lastId={
+              reviewList.reduce((max, review) => review.id > max ? review.id : max, 0 ) 
+            }
+            onAddReview={
+              (myReview) => { setReviewList([...reviewList, myReview]) }} />
+        </div>
+
+      </article>      
 
     </article>
   )  
