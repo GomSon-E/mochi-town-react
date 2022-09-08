7fd69f44b4bf83dc98caf5175817a1161e9ef622
GomSon-E <gomsonixx@gmail.com>
2022-09-08 16:11:06 +0900

    Update Header.js - a href

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,18 +7,18 @@ function Header() {
       <div>
         <p id="menuBtn"><span></span><span></span><span></span></p>
         <h1>
-          <a href="#">
+          <a href={()=>false}>
             <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="모찌타운" />
           </a>
         </h1>
         <nav>
           <h2 className="hide">메인네비게이션</h2>
           <ul id="gnb">
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">MAIN</a></li>
+            <li><a href={()=>false}>ABOUT</a></li>
+            <li><a href={()=>false}>MAIN</a></li>
             <li><Link to="/">SHOP</Link></li>
             <li><Link to="/review">REVIEW</Link></li>
-            <li><a href="#">LOGIN</a></li>
+            <li><a href={()=>false}>LOGIN</a></li>
           </ul>
           <h2 className="hide">검색창</h2>
           <div id="search">
