a4e9eabbd2389e0e4604c9e43c89a3c8a50e722d
GomSon-E <gomsonixx@gmail.com>
2022-09-16 00:52:46 +0900

    Update Products.js - JQuery : img hover

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,8 +1,27 @@
+import $ from "jquery"
+
+function handleMouseEnter (event) {
+  const sel = $(event.target).parents('li.productWrap').children('ul.productBtns')
+  $('li:first-child', sel).animate({top: "0px"}, 200)
+  $('li.like', sel).animate({right: "0px"}, 200)
+  $(sel).css('display', 'block')
+}
+
+function handleMouseLeave () {
+  $('ul.productBtns>li:first-child', this).css('top', '50px')
+  $('ul.productBtns li.like', this).css('right', '-70px')
+  $('ul.productBtns', this).css('display', 'none')
+}
+
 function Products({product}) {
   return (
     <li className="productWrap">
       <figure className="productList">
-        <img src={`${process.env.PUBLIC_URL}/products/${product.img}.jpg`} alt={product.name} />
+        <img 
+          src={`${process.env.PUBLIC_URL}/products/${product.img}.jpg`} 
+          alt={product.name} 
+          onMouseEnter={handleMouseEnter} 
+          onMouseLeave={handleMouseLeave} />
         <figcaption>
           <dl>
             <dt>{product.name}</dt>
@@ -11,7 +30,8 @@ function Products({product}) {
           </dl>
         </figcaption>
       </figure>
-      <ul className="productBtns">
+      <ul className="productBtns"
+        onMouseEnter={handleMouseEnter}>
         <li>
           <ul>
             <li className="productBtn cart">장바구니</li>
